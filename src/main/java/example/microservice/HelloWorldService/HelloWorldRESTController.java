4068e72e2f70430ef2c2873a9febc423b9812b22
louie1711 <louis.moloney@gmail.com>
2021-10-21 18:21:45 +0100

    Service for microservice tutorial

---
+++ src/main/java/example/microservice/HelloWorldService/HelloWorldRESTController.java
@@ -0,0 +1,16 @@
+package example.microservice.HelloWorldService;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient 
+public class HelloWorldRESTController {
+
+	@GetMapping("/helloworldrest")
+	public HelloWorldWrapper hello() {
+		return new HelloWorldWrapper();
+	}
+	
+}
---
+++ src/main/java/example/microservice/HelloWorldService/HelloWorldWrapper.java
@@ -0,0 +1,15 @@
+package example.microservice.HelloWorldService;
+
+public class HelloWorldWrapper {
+	
+	private String message = "I am the hello world object ";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
